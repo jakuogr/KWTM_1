ff764f3ab98bc57b5a5486d4944ddfcceb9df005
jakuogr <81562373+jakuogr@users.noreply.github.com>
2022-03-17 17:57:58 +0100

    Trzeci commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-include<trygonometria.h>
+#include<LaborkaConfig.h>
+#include<trygonometria.h>
 #include<cmath>
 #include<iostream>
 int main() {
@@ -6,5 +7,16 @@ int main() {
 	double cos = degreemath::cos(90);
 	double tan = degreemath::tan(90);
 	double ctg = degreemath::ctg(90);
+#ifdef USE_TRIGONOMETRY_DEGREE
+    double sinTT = degreemath::sin(90);
+    double cosTT = degreemath::cos(90);
+    double tanTT = degreemath::tan(90);
+    double ctgTT = degreemath::ctg(90);
+#else
+    double sinTF = sin(90);
+    double cosTF = cos(90);
+    double tanTF = tan(90);
+    double ctgTF = 1 / tan(90);
+#endif
 	return 0;
 }
