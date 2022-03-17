d14b3751f1374bc1f9d21a2b2f260c5ab0482749
jakuogr <81562373+jakuogr@users.noreply.github.com>
2022-03-17 17:31:17 +0100

    Drugi commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,10 @@
-int main() { /*...*/ return 0; }
+include<trygonometria.h>
+#include<cmath>
+#include<iostream>
+int main() {
+	double sin = degreemath::sin(90);
+	double cos = degreemath::cos(90);
+	double tan = degreemath::tan(90);
+	double ctg = degreemath::ctg(90);
+	return 0;
+}
